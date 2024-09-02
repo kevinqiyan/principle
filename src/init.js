a2d78fe1de53b1801c6cb93c675d2491a919d65e
kevin <kevin1025253@gmail.com>
2024-09-03 00:18:24 +0800

    20240903练习

--- src/init.js
+++ src/init.js
@@ -1,26 +1,22 @@
-// 给 Vue 增加init 方法
 export function initMixin(Vue) {
-
     Vue.prototype._init = function (options) {
         const vm = this
         vm.$options = options
-        debugger
+        // 初始化数据
         initState(vm)
     }
 }
-function initState(vm) {
-    const opts = vm.$options
 
-    if (opts.data) {
+// init state
+function initState(vm) {
+    const opt = vm.$options // 获取所有的选项
+    if (opt.data) {
         initData(vm)
     }
-    
 }
 
 function initData(vm) {
-
     let data = vm.$options.data
     data = typeof data === 'function' ? data.call(vm) : data
-
     console.log('data', data);
 }
